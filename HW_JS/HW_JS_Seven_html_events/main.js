08e1e109ff145fb128e41bf6d4bd861ac7a2b713
IhnatovychYura <yuraihnatovych111@gmail.com>
2020-11-23 03:55:59 +0200

    add HW2

---
+++ HW_JS/HW_JS_Seven_html_events/main.js
@@ -0,0 +1,140 @@
+console.log(22);
+console.log("Task 1 ______________________________________________________________");
+// - Создать произвольный елемент с id = text.  Используя JavaScript, сделайте так, чтобы при клике на кнопку исчезал элемент с id="text".
+
+let div = document.createElement('div');
+div.id = 'text';
+div.innerText = 'Divka which can delete herself';
+div.style.backgroundColor = 'lightgreen';
+
+let btn = document.createElement('button');
+btn.innerText = 'DELETE ME';
+
+div.appendChild(btn);
+document.body.appendChild(div);
+
+btn.onclick = (ev)=>{
+    console.log(ev.target.parentElement);
+    ev.target.parentElement.style.display = 'none'
+}
+
+console.log("Task 2 ______________________________________________________________");
+// - Создайте кнопку, при клике на которую, она будет скрывать сама себя.
+let divTwo = document.createElement('div');
+divTwo.innerText = 'Divka which can delete own btn';
+divTwo.style.backgroundColor = 'lightyellow';
+
+let btnTwo = document.createElement('button');
+btnTwo.innerText = 'DELETE ME, real me';
+
+divTwo.appendChild(btnTwo);
+document.body.appendChild(divTwo);
+
+btnTwo.onclick = ()=>{btnTwo.style.display = 'none'}
+
+console.log("Task 3______________________________________________________________");
+// - створити інпут який приймає вік людини та кнопку яка підтверджує дію.При натисканні на кнопку зчитати інформацію з інпуту та перевірити вік чи меньше він ніж 18, та повідомити про це користувача
+let divThree = document.createElement('div');
+divThree.innerText = 'Enter your age: ';
+divThree.style.backgroundColor = 'lightblue';
+
+let input = document.createElement('input');
+input.value ='18';
+
+let confirm = document.createElement('button');
+confirm.innerText = 'confirm';
+
+divThree.appendChild(input);
+divThree.appendChild(confirm);
+document.body.appendChild(divThree);
+
+confirm.onclick = () => {
+    console.log(input.value)
+    input.value >= 18 ? alert('Вітаю, Ви повнолітній') : alert('Шкода, Ви неповнолітній, тому геть з нашого сайту')
+}
+
+console.log("Task 4______________________________________________________________");
+// - Создайте меню, которое раскрывается/сворачивается при клике
+let divFour = document.createElement('div');
+divFour.style.backgroundColor = 'lightgrey';
+
+let menu = document.createElement('button');
+menu.innerText = 'МЕНЮ';
+
+menu.onclick = () => {
+
+}
+
+divFour.appendChild(menu);
+document.body.appendChild(divFour);
+
+
+
+// - Создать список комментариев , пример объекта коментария - {title : 'lorem', body:'lorem ipsum dolo sit ameti'}.
+//     Вывести список комментариев в документ, каждый в своем блоке.
+//     Добавьте каждому комментарию по кнопке для сворачивания его body.
+//
+// - створити 2 форми  по 2 інпути в кожній. ствоирити кнопку при кліку на яку считується та виводиться на консоль інформація з цих 2х форм.
+//     Кнопка повинна лежати за межами форм (Щоб ьуникнути  перезавантаження сторінки)
+// Доступ до інпутів через Forms API. Отже дайте формі та інпутам всі необхідні атрибути.
+//
+// - Створити функцію, яка генерує таблицю.
+//     Перший аргумент визначає кількість строк.
+//     Другий параметр визначає кліькіть ячеєк в кожній строці.
+//     Третій параметр визначає елемент в який потрібно таблицю додати.
+//
+//
+// - Створити 3 инпута та кнопку. Один визначає кількість рядків, другий - кількість ячеєк, третій вмиіст ячеєк.
+//     При натисканні кнопки, вся ця інформація зчитується і формується табличка, з відповідним вмістом.
+// (Додатковачастина для завдання)
+//
+// - Напишите «Карусель» – ленту изображений, которую можно листать влево-вправо нажатием на стрелочки.
+//
+//
+// - Сворити масив не цензцрних слів.
+//     Сворити інпут текстового типу.
+//     Якщо людина вводить слово і воно міститься в масиві не цензурних слів
+// кинути алерт з попередженням.
+//     Перевірку робити при натисканні на кнопку
+//
+//
+// - Сворити масив не цензцрних слів.
+//     Сворити інпут текстового типу.
+//     Потрібно перевіряти чи не містить ціле речення в собі погані слова.
+//     Кинути алерт з попередженням у випадку якщо містить.
+//     Перевірку робити при натисканні на кнопку
+//
+//
+//
+// -- создать скрипт, который берет считывает на странице (rules.html) текст и делает сбоку меню-оглавление по всем заголовкам которые есть в тексте.
+//     При клике на пункт оглавления вы должны отправляться к этому пункту в тексте
+//
+// -- взять массив пользователей
+// let usersWithAddress = [
+//     {id:1,name: 'vasya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
+//     {id:2,name: 'petya', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 1}},
+//     {id:3,name: 'kolya', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 121}},
+//     {id:4,name: 'olya', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 90}},
+//     {id:5,name: 'max', age: 30, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 115}},
+//     {id:6,name: 'anya', age: 31, status: false, address: {city: 'Kyiv', street: 'Shevchenko', number: 2}},
+//     {id:7,name: 'oleg', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 22}},
+//     {id:8,name: 'andrey', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 43}},
+//     {id:9,name: 'masha', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 12}},
+//     {id:10,name: 'olya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
+//     {id:11,name: 'max', age: 31, status: true, address: {city: 'Ternopil', street: 'Shevchenko', number: 121}}
+// ];
+// Создать три чекбокса. Каждый из них активирует фильтр для вышеуказаного массива. Фильтры могут работать как вместе так и по отдельности.
+// 1й - отфильтровывает пользователей со статусом false (осталяет со статусом false)
+// 2й - оставляет старше 29 лет включительно
+// 3й - оставляет тех у кого город киев
+// Данные выводить в документ
+//
+//
+//
+// *****(Прям овердоз с рекурсией) Создать функцию которая принимает какой-либо элемент DOM-структуры .Функция создает в боди 2 кнопки (назад/вперед)
+// при нажатии вперед, вы переходите к дочернему элементу, при еще одном нажатии на "вперед", вы переходите к следующему дочернему элементу (лежащему на одном уровне)
+// НО если у (какого-либо)дочеренего элемента есть дети, то нажатие "вперед" позволяет нам войти внутрь элемента и  выводит первого ребенка. и тд.
+//     Когда все дети заканчиваются, мы выходим из данного дочернего элемента и переходим к следующему, лежащему с ним на одном уровне
+//
+//
+// *** При виділені сегменту тексту на сторінці він стає жирний/курсивний/або якось іншим способом змінює свій стан
