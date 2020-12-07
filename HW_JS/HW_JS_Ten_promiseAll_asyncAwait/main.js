00e5a51ce81aea01d2139a5e00d1d71549683942
IhnatovychYura <yuraihnatovych111@gmail.com>
2020-12-07 17:13:39 +0200

    add HW2

--- HW_JS/HW_JS_Ten_promiseAll_asyncAwait/main.js
+++ HW_JS/HW_JS_Ten_promiseAll_asyncAwait/main.js
@@ -1,4 +1,5 @@
 console.log(22)
+console.log("Task 1______________________________________________________________");
 // Реалізувати друкарську машинку.
 //     У вас є текст "Hello World". (або юудь який інший на ваш розсуд)
 // Ваша функція має друкувати цей текст по 1 симоволу в браузері.
@@ -22,8 +23,8 @@ function showText(letter) {
         setTimeout(() => {
             if (letter === ' ') {
                 resolve(txt.innerHTML += `&ensp;`)
-            } else if(letter){
-                resolve( txt.innerText +=`${letter}`)
+            } else if (letter) {
+                resolve(txt.innerText += `${letter}`)
                 return
             }
             reject("Something go wrong")
@@ -33,9 +34,75 @@ function showText(letter) {
 }
 
 async function catchLetter(arrayOfLetters) {
-   for (let i=0; i<arrayOfLetters.length; i++){
-       await showText(arrayOfLetters[i])
-   }
+    for (let i = 0; i < arrayOfLetters.length; i++) {
+        await showText(arrayOfLetters[i])
+    }
 }
 
 catchLetter(arrString);
+console.log("Task 2______________________________________________________________");
+
+// function userCard(number) {
+//
+//     return {
+//         balance: 100,
+//         transactionLimit: 100,
+//         historyLogs: [],
+//         key: number,
+//     }
+// }
+
+function userCard(number) {
+    let _balance = 100
+    let _transactionLimit = 100
+    let _historyLogs = []
+    let _key = number
+
+   function getCardOptions(){
+       return {
+        balance: _balance,
+        transactionLimit: _transactionLimit,
+        historyLogs: _historyLogs,
+        key: _key,
+        }
+   }
+
+   function putCredits(money) {
+        _balance += money;
+   }
+
+    function takeCredits(money) {
+        if (money <= _transactionLimit && _balance >= money){
+            _balance -= money;
+        } else if (money >= _transactionLimit) {
+            console.error(`ПОМИЛКА, Ви бажаєте зняти кошти сумою яка більша за Ваш кредитний ліміт ${_transactionLimit} грн`)
+        } else if (_balance <= money) {
+            console.error(`ПОМИЛКА, У Вас недостатньо коштів на рахунку ${_balance} грн`)
+        }
+    }
+
+    function setTransactionLimit(limit) {
+        _transactionLimit = limit;
+    }
+
+    return {
+        getCardOptions,
+        putCredits,
+        takeCredits,
+    }
+}
+
+const card2 = new userCard(2);
+const card3 = new userCard(3);
+
+
+
+
+
+
+
+
+
+
+
+
